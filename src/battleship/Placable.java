3ee7d1c66f1eea16c5cad733f1459d50e5580c30
Mohammad Amin Khashkhashi Moghaddam <amin.moghaddamV@gmail.com>
2014-11-29 11:07:39 +0330

    Changed Placable interface to Placable abstract class

--- src/battleship/Placable.java
+++ src/battleship/Placable.java
@@ -3,6 +3,31 @@ package battleship;
 /**
  * Created by amin on 11/29/14.
  */
-interface Placable {
-    boolean place(int x,int y,Square[][] map);
+abstract class PlacableWeaponry extends Weaponry
+{
+    private int x;
+    private int y;
+
+    public PlacableWeaponry(int id, int health, int x, int y, Square[][] map)
+    {
+        super(id, health);
+        this.x = x;
+        this.y = y;
+        place(x, y, map);
+    }
+
+    public int getX()
+    {
+
+        return x;
+    }
+
+
+    public int getY()
+    {
+        return y;
+    }
+
+
+    abstract boolean place(int x, int y, Square[][] map);
 }
