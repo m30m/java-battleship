86189feda2c6a46905c52cf9f3a0aa417cd8a366
Seyed Ali Tabatabaee <The_CodeBreakeR@Seyeds-MacBook-Pro.local>
2014-12-27 12:06:03 +0330

    implemented staticReader method in ConsoleRunner

--- src/battleship/console/ConsoleRunner.java
+++ src/battleship/console/ConsoleRunner.java
@@ -1,5 +1,7 @@
 package battleship.console;
 
+import java.util.Scanner;
+
 import battleship.Player;
 import battleship.Runner;
 
@@ -8,17 +10,46 @@ import battleship.Runner;
  */
 public class ConsoleRunner extends Runner
 {
-    void mineTrap(Player player, int x, int y)
+	
+	void staticReader(Scanner input, Player player)
+	{
+		String str=null, equipment="Battleship";
+		int[] lengthOfBattleship={4, 3, 3, 2, 2, 1, 1};
+		int numOfBattleship=0;
+		while(!str.equals("DONE"))
+		{
+			str=input.nextLine();
+			if(str.equals("Anti aircraft") || str.equals("Mine"))
+			{
+				equipment=str;
+				continue;
+			}
+			str.replace(',', ' ');
+			Scanner scanner=new Scanner(str);
+			if(equipment.equals("Battleship"))
+			{
+				new Battleship(0, player, scanner.nextInt(), scanner.nextInt(), 
+						lengthOfBattleship[numOfBattleship], scanner.nextByte()=='V');
+				numOfBattleship++;
+			}
+			else if(equipment.equals("Anti aircraft"))
+				new AntiAircraft(0, player, scanner.nextInt());
+			else
+				new Mine(0, player, scanner.nextInt(), scanner.nextInt());
+		}
+	}
+	
+    public void mineTrap(Player player, int x, int y)
     {
         System.out.println("team "+player.getName()+" mine trap "+x+","+y);
     }
 
-    void explode(Player player,int x, int y)
+    public void explode(Player player,int x, int y)
     {
         System.out.println("team "+player.getName()+" explode "+x+","+y);
     }
 
-    void aircraftUnsuccessful(Player player)
+    public aircraftUnsuccessful(Player player)
     {
         System.out.println("aircraft unsuccessful");
     }
