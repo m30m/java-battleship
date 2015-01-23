f99d0103555b6d32173fc4ed2086e2eeb8debafa
Seyed Ali Tabatabaee <The_CodeBreakeR@Seyeds-MacBook-Pro.local>
2015-01-24 01:42:15 +0330

    some graphic improvements...

--- src/battleship/GraphicSquare.java
+++ src/battleship/GraphicSquare.java
@@ -2,10 +2,14 @@ package battleship;
 
 import battleship.console.GraphicRunner;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by amin on 1/20/15.
@@ -16,11 +20,24 @@ public class GraphicSquare extends JComponent
     Square square;
     GraphicRunner runner;
 
+    private static BufferedImage mine0, mine1, antiaircraft;
+
+    static {
+        try {
+            mine0 = ImageIO.read(new File("src/images/mine0.png"));
+            mine1 = ImageIO.read(new File("src/images/mine1.png"));
+            antiaircraft = ImageIO.read(new File("src/images/antiaircraft.png"));
+        } catch (IOException e) {
+
+        }
+    }
+
+
     public GraphicSquare(Square square, final GraphicRunner runner)
     {
         this.runner = runner;
         this.square = square;
-        int padding = 6;
+        int padding = 2;
         setBounds(square.getX() * SIZE + padding / 2, square.getY() * SIZE + padding / 2, SIZE - padding, SIZE - padding);
         addMouseListener(new MouseListener()
         {
@@ -63,32 +80,35 @@ public class GraphicSquare extends JComponent
     }
 
     @Override
-    protected void paintComponent(Graphics g)
-    {
+    protected void paintComponent(Graphics g) {
         super.paintComponents(g);
         Graphics2D g2d = (Graphics2D) g;
         g2d.setColor(new Color(255, 230, 225));
-        if(runner.getState()==GameState.TeamAPlaying || runner.getState()==GameState.TeamBPlaying) {
-            if(square.isDestroyed())
+        if (runner.getState() == GameState.TeamAPlaying || runner.getState() == GameState.TeamBPlaying) {
+            if (square.isDestroyed())
                 g2d.setColor(new Color(0, 0, 0));
-            else if(square.isDetected())
-            {
+            else if (square.isDetected()) {
                 g2d.setColor(new Color(153, 230, 255));
-                if(square.getPlacableWeaponry() instanceof Battleship)
+                if (square.getPlacableWeaponry() instanceof Battleship)
                     g2d.setColor(new Color(23, 255, 139));
             }
 
-        }
-        else {
+        } else {
             if (square.getPlacableWeaponry() == null)
                 g2d.setColor(new Color(153, 230, 255));
-            else if (square.getPlacableWeaponry() instanceof AntiAircraft)
-                g2d.setColor(new Color(222, 255, 76));
+            else if (square.getPlacableWeaponry() instanceof AntiAircraft){
+                Color c = g2d.getColor();
+                g2d.drawImage(antiaircraft, 0, 0, null);
+                g2d.setColor(new Color(c.getRed(), c.getGreen(), c.getBlue(), 0));
+            }
             else if (square.getPlacableWeaponry() instanceof Battleship)
                 g2d.setColor(new Color(23, 255, 139));
-            else if (square.getPlacableWeaponry() instanceof Mine)
-                g2d.setColor(new Color(170, 54, 255));
+            else if (square.getPlacableWeaponry() instanceof Mine) {
+                Color c = g2d.getColor();
+                g2d.drawImage(mine1, 0, 0, null);
+                g2d.setColor(new Color(c.getRed(), c.getGreen(), c.getBlue(), 0));
+            }
+            g2d.fillRect(0, 0, SIZE, SIZE);
         }
-        g2d.fillRect(0, 0, SIZE, SIZE);
     }
 }
